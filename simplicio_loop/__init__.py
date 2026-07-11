36328e334bafe5f2fe0cf0df4a0b4c04acd520b5
Wesley Simplicio <wesleysimplicio@live.com>
2026-07-11 11:36:04 -0300

    chore: release simplicio-loop 3.32.0 (#227)
    Co-authored-by: Simplicio, Wesley (ext) <wesley.simplicio.ext@siemens-energy.com>

--- simplicio_loop/__init__.py
+++ simplicio_loop/__init__.py
@@ -13,6 +13,6 @@ try:
     try:
         __version__ = _v("simplicio-loop")
     except PackageNotFoundError:
-        __version__ = "3.31.1"
+        __version__ = "3.32.0"
 except Exception:  # pragma: no cover
-    __version__ = "3.31.1"
+    __version__ = "3.32.0"
