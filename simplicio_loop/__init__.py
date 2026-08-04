005263ba224a69ce4651e68eae06fefca3c3f5a8
Simplicio, Wesley (ext) <wesley.simplicio.ext@siemens-energy.com>
2026-08-03 22:20:39 -0300

    chore(release): v3.38.36 — Prism slots machine-max (CPU+RAM)

--- simplicio_loop/__init__.py
+++ simplicio_loop/__init__.py
@@ -13,6 +13,6 @@ try:
     try:
         __version__ = _v("simplicio-loop")
     except PackageNotFoundError:
-        __version__ = "3.38.35"
+        __version__ = "3.38.36"
 except Exception:  # pragma: no cover
-    __version__ = "3.38.35"
+    __version__ = "3.38.36"
