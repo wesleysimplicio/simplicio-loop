f32b750569d9f21f29d8ace0f143da4db4a68f10
Wesley Simplicio <wesleysimplicio@live.com>
2026-08-10 11:24:55 -0300

    Keep Loop fallback version aligned

--- simplicio_loop/__init__.py
+++ simplicio_loop/__init__.py
@@ -13,7 +13,6 @@ try:
     try:
         __version__ = _v("simplicio-loop")
     except PackageNotFoundError:
-        __version__ = "3.42.0"
+        __version__ = "3.43.0"
 except Exception:  # pragma: no cover
-    __version__ = "3.42.0"
-
+    __version__ = "3.43.0"
